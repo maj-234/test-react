9c0c5c94203abc5ba2fbe7a2778704e38bf6f4c4
rack <maj@rack.cloud>
2024-05-30 07:53:07 +0000

    update

--- src/component/Game.js
+++ src/component/Game.js
@@ -1,40 +1,72 @@
 import {useState} from 'react';
 
-function Square({value}) {
-    return <button className="square">{ value }</button>;
+function Square({value, click}) {
+    return <button className="square" onClick={click}>{ value }</button>;
 }
 
-function Board({squares}) {
+function Board({squares, isX, update}) {
+
+    function click(n) {
+        if (squares[n]) {
+            return;
+        }
+        else {
+            const nextSquares = squares.slice()
+            if (isX) {
+                nextSquares[n] = 'X';
+                console.log('clicked');
+            }
+            else {
+                nextSquares[n] = 'O';
+            }
+            update(nextSquares);
+        }
+    }
+
     return (
         <>
             <div className="board-row">
-                <Square value='1'/>
-                <Square value='2'/>
-                <Square value='3'/>
+                <Square value={squares[0]} click={() => click(0)} />
+                <Square value={squares[1]} click={() => click(1)}/>
+                <Square value={squares[2]} click={() => click(2)}/>
             </div>
             <div className="board-row">
-                <Square value='3'/>
-                <Square value='3'/>
-                <Square value='3'/>
+                <Square value={squares[3]} click={() => click(3)}/>
+                <Square value={squares[4]} click={() => click(4)}/>
+                <Square value={squares[5]} click={() => click(5)}/>
             </div>
             <div className="board-row">
-                <Square value='3'/>
-                <Square value='3'/>
-                <Square value='3'/>
+                <Square value={squares[6]} click={() => click(6)}/>
+                <Square value={squares[7]} click={() => click(7)}/>
+                <Square value={squares[8]} click={() => click(8)}/>
             </div>
         </>
     );
 }
 
 function Game() {
-    const [history, setHistory] = useState([Array(9).fill('null')]);
-    const [currentMove, setCurrentMove] = useState(0)
-    const currentSquare = history[currentMove]
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [currentMove, setCurrentMove] = useState(0);
+    const currentSquare = history[currentMove];
+    const isX = currentMove % 2 === 0;
+
+    function update(nextSquares) {
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
+    }
 
     const move = history.map((e, i) => {
+        let info;
+        if (i === 0) {
+            info = 'back to start';
+        }
+        else {
+            info = 'back to #' + i;
+        }
         return (
             <li key={i}>
-                <button>{i}</button>
+                <button>{info}</button>
             </li>
         )
     });
@@ -42,7 +74,7 @@ function Game() {
     return (
         <div className='game'>
             <div className='game-board'>
-                <Board squares={currentSquare}/>
+                <Board squares={currentSquare} isX={isX} update={update}/>
             </div>
             <div className='game-info'>
                 {move}
